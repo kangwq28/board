419610016282c5bf0fbbb38a183677e4afb3e6e4
rizk <rizk303333@gmail.com>
2020-07-08 07:31:32 +0900

    7/8 댓글 작성 테스트 완료

--- src/main/java/kr/co/controller/BoardController.java
+++ src/main/java/kr/co/controller/BoardController.java
@@ -1,5 +1,7 @@
 package kr.co.controller;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
@@ -12,8 +14,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import kr.co.service.BoardService;
+import kr.co.service.ReplyService;
 import kr.co.vo.BoardVO;
 import kr.co.vo.PageMaker;
+import kr.co.vo.ReplyVO;
 import kr.co.vo.SearchCriteria;
 
 @Controller
@@ -24,6 +28,9 @@ private static final Logger logger = LoggerFactory.getLogger(BoardController.cla
 	
 	@Inject
 	BoardService service;
+	
+	@Inject
+	ReplyService replyService;
 	
 	// 게시판 글 작성 화면
 	@RequestMapping(value = "/board/writeView", method = RequestMethod.GET)
@@ -65,6 +72,9 @@ private static final Logger logger = LoggerFactory.getLogger(BoardController.cla
 			
 			model.addAttribute("read", service.read(boardVO.getBno()));
 			model.addAttribute("scri", scri);
+			
+			List<ReplyVO> replyList = replyService.readReply(boardVO.getBno());
+			model.addAttribute("replyList", replyList);
 			return "board/readView";
 			
 		}
